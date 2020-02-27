87b04f402ca94e22c6f5353cb32b4f0167af50ff
hover <113245756@qq.com>
2020-02-27 20:53:48 +0800

    增加了点击登录，获得文本框中的用户名和密码，发送给后端（成功传递后怎么获取返回值还未编写）

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -6,128 +6,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-    realUrl: '',
-    nickName: '', //用户的名称
-    avatarUrl: '', //用户的头像
-    saveUserInfo: {
-      username: "",
-      password: ""
-    }
+    username:"",
+    password:"",
+    endUsername:"",
+    endPassword:""
   },
-  onGotUserInfo: function(e) {
-    // console.log("nickname=" + e.detail.userInfo.nickName);
-    // console.log("avatarUrl=" + e.detail.userInfo.avatarUrl);
-    // console.log("gender=" + e.detail.userInfo.gender);
-    // console.log("city=" + e.detail.userInfo.city);
-    // console.log("province=" + e.detail.userInfo.province);
-    // console.log("country=" + e.detail.userInfo.country);
-    var rawData = JSON.parse(e.detail.rawData);
-    this.setData({
-      nickName: e.detail.userInfo.nickName,
-      avatarUrl: e.detail.userInfo.avatarUrl,
-    })
-  },
-  goRegist: function() {
-    wx.navigateTo({
+  goRegist : function(){
+    wx.redirectTo({
       url: '../userRegist/regist',
     })
   },
-  doLogin: function(e) {
-    var me = this;
-    var formObject = e.detail.value;
-    var username = formObject.username;
-    var password = formObject.password;
-    //简单验证
-    if (username.length == 0 || password.length == 0) { //反馈数据
-      wx.showToast({
-        title: '你好,用户名和密码不能为空哦',
-        icon: 'none', //图标
-        duration: 3000
-      })
-    } else {
-      var serverUrl = app.serverUrl;
-      wx.showLoading({
-        title: '请等待...',
-      })
-      wx.request({
-        url: serverUrl + '/login',
-        method: "POST",
-        data: {
-          username: username,
-          password: password
-        },
-        header: {
-          'content-type': 'application/json' //默认值
-        },
-        //回调函数 
-        //返回
-        success: function(res) {
-          var status = res.data.status;
-          if (status == 200) {
-            wx.hideLoading();
-            wx.showToast({
-              title: '登陆成功啦',
-              icon: 'none',
-              duration: 3000
-            })
-            var saveUserInfo = me.data.saveUserInfo;
-            saveUserInfo.password = password;
-            saveUserInfo.username = username;
-            app.saveUserInfo(saveUserInfo);
-            app.setGlobalUserInfo(res.data.data);
-            var realUrl = me.data.realUrl;
-            console.log("realUrl" + realUrl);
-            realUrl = app.globalData.realUrl;
-            console.log("publisherId" + app.globalData.publisherId);
-            if (realUrl == '' || realUrl == null) {
-              //跳转
-              wx.switchTab({
-                url: '../release/release',
-              })
-            } else {
-              wx.redirectTo({
-                url: realUrl
-              })
-            }
-          } else if (status == 500) {
-            //失败 弹出框
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 3000
-            })
-          }
-        }
-      })
-    }
+  //提交功能绑定在form表单上即<form bindsubmit='doLogin'>中的doLogin
+  doLogin:function(e){
+    var self = this;
+    self.setData({
+      username: e.detail.value.username,
+      password: e.detail.value.password,
+    })
+    wx.request({
+      url: 'http://localhost:8080/Test',
+      //url: 'https://www.plantdisrecogn.com/wxupload/goods/getUser',//自己请求的服务器的地址
+      method: 'POST',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (req) {
+        console.log(req.data);
+        self.setData({
+        })
+
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log("result：" + app.globalData.realUrl);
-    var me = this;
-    var realUrl = e.realUrl;
-    console.log(realUrl);
-    if (realUrl != null && realUrl != '' && realUrl != undefined) {
-      realUrl = realUrl.replace('@', '=');
-      realUrl = realUrl.replace('#', '?');
-      //../videoinfo/videoinfo#videoInfo@[object Object]
-      console.log("真实的路径" + realUrl);
-      me.setData({
-        realUrl: realUrl
-      })
-    }
-    var user = app.getSaveUserInfo();
-    var username = user.username;
-    var password = user.pasword;
-    if (username != null && username != undefined && username != '' && password != null && password != undefined &&
-      password != '') {
-      me.setData({
-        username: username,
-        password: password
-      })
-    }
+    
   },
 
   /**
--- pages/userRegist/regist.js
+++ pages/userRegist/regist.js
@@ -3,9 +3,15 @@ const app = getApp()
 Page({
   data: {
 
-  },//点击事件
+  },
+  goLoginPage: function () {
+    wx.switchTab({
+      url: '../publish/publish',
+    })
+  },
+  //点击事件
   chooseImg: function () {
-    var that = this
+    var self = this
     wx.chooseImage({
       count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -13,21 +19,21 @@ Page({
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths
-        that.setData({
+        self.setData({
           photos: tempFilePaths
         })
-        console.log(that.data.photos)
+        console.log(self.data.photos)
       }
     })
   },
 
   uploadImg: function () {
-    var that = this
+    var self = this
     wx.uploadFile({
 
-      url: 'https://www.plantdisrecogn.com/wxupload/goods/upload', //仅为示例，非真实的接口地址
-     // url: 'http://localhost:8080/goods/upload',
-      filePath: that.data.photos[0],
+      url: 'http://localhost:8080/queryPost', //仅为示例，非真实的接口地址
+     // url: 'http://localhost:8080/queryPost',
+      filePath: self.data.photos[0],
       name: 'file',
       formData: {
         'user': '大豆',
@@ -40,67 +46,6 @@ Page({
           url: '../wait/wait',
         })
       }
-    })
-  },
-  doRegist: function(e) {
-    var formObject = e.detail.value;
-    var username = formObject.username;
-    var password = formObject.password;
-    //简单验证
-    if (username.length == 0 || password.length == 0) { //反馈数据
-      wx.showToast({
-        title: '用户名或者密码不能为空',
-        icon: 'none', //图标
-        duration: 3000
-      })
-    } else {
-      var serverUrl = app.serverUrl;
-      wx.showLoading({
-        title: '请等待...',
-      })
-      wx.request({
-        url: serverUrl + '/regist',
-        method: "POST",
-        data: {
-          username: username,
-          password: password
-        },
-        header: {
-          'content-type': 'application/json' //默认值
-        },
-        //回调函数 
-        success: function(res) {
-          var status = res.data.status;
-          wx.hideLoading();
-
-          if (status == 200) {
-            wx.showToast({
-              title: '注册成功啦~~',
-              icon: 'none',
-              duration: 3000
-            })
-            app.setGlobalUserInfo(res.data.data);
-            //页面跳转
-            wx.navigateTo({
-              url: '../userLogin/login',
-            })
-
-
-          } else if (status = 500) {
-            wx.showToast({
-              title: res.data.msg,
-              icon: 'none',
-              duration: 3000
-            })
-          }
-        }
-      })
-    }
-  },
-  goLoginPage: function() {
-    console("跳转至登陆页面"),
-    wx.navigateTo({
-      url: '../publish/publish',
     })
   }
 })
