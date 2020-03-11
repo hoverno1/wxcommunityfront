9a6f06bf60c4fa96279be8bc9b1fd11e42ddb922
hover <113245756@qq.com>
2020-03-11 21:46:47 +0800

    实现了详情页发布功能，最后只需完成图片显示处理

--- pages/publishPost/publishPost.js
+++ pages/publishPost/publishPost.js
@@ -5,7 +5,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-    aid:null,
+  },
+
+doRegist: function(e){
+  var self = this;
+  self.setData({
+    disease: e.detail.value.disease,
+    diseaseContent: e.detail.value.diseaseContent,
+  })
+  wx.request({
+    url: 'http://localhost:8080/publishPost',
+    //url: 'https://www.plantdisrecogn.com/wxupload/goods/getUser',//自己请求的服务器的地址
+    method: 'POST',
+    data: {
+      aid: self.data.aid,
+      disease: self.data.disease,
+      diseaseContent: self.data.diseaseContent,
+    },
+    header: {
+      'content-type': 'application/x-www-form-urlencoded' // 默认值 上传用这个类型好
+    },
+    success: function (req) {
+      //console.log(req.data);
+      self.setData({
+        status: req.data,
+      })
+    }
+  })
+  wx.uploadFile({
+
+    url: 'http://localhost:8080/publishPostPic', //仅为示例，非真实的接口地址
+    // url: 'http://localhost:8080/queryPost',
+    filePath: self.data.photos[0],
+    name: 'file',
+    success: function (res) {
+      var sta = res.data
+      //console.log(res)
+      self.setData({
+        status2: sta,
+      })
+    }
+  })
+
+  //等待一会再执行
+  setTimeout(function () {
+    //判断登陆信息，信息正确则status与status2值为1，跳转发布页面
+    //status是判断用户名与密码，status2是判断头像
+    if (self.data.status == 1 && self.data.status2 == 1) {
+      wx.switchTab({
+        url: '../community/community',
+      })
+    } else {
+      if (self.data.status != 1) {
+        wx.showToast({
+          title: '文字内容有误',
+          icon: 'none',
+          duration: 2000
+        })
+      } else {
+        wx.showToast({
+          title: '图片有误',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
+  },
+    //睡2秒
+    3000)
+
+},
+  chooseImg: function () {
+    var self = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        self.setData({
+          photos: tempFilePaths
+        })
+      },
+    })
   },
 
   /**
